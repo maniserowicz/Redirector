c2f86d797aa8bb1edf6d99fcdd91dd94800abbd3
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2013-04-22 06:59:50 +0200

    introduced spec context "making_request"

---
+++ src/Redirector.Tests.Acceptance/service/contexts/making_request.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http.SelfHost;
+using Machine.Specifications;
+using Procent.Redirector.Configuration;
+
+namespace Procent.Redirector.Tests.Acceptance.service
+{
+    public class making_request
+    {
+        Establish ctx = () =>
+            {
+                var address = new Uri("http://localhost:8355");
+                var config = new HttpSelfHostConfiguration(address);
+
+                WebApiBootstrap.Configure(config);
+
+                var server = new HttpSelfHostServer(config);
+
+                _httpClient = new HttpClient(server);
+                _httpClient.BaseAddress = address;
+            };
+
+        Because of = () => _response = _httpClient.GetAsync(_url).Result;
+
+        Cleanup client = () => _httpClient.Dispose();
+
+        static HttpClient _httpClient;
+
+        protected static string _url;
+        protected static HttpResponseMessage _response;
+    }
+}
--- src/Redirector.Tests.Acceptance/service/requesting_root.cs
+++ src/Redirector.Tests.Acceptance/service/requesting_root.cs
@@ -1,32 +1,12 @@
-﻿using System;
-using System.Net.Http;
-using System.Web.Http.SelfHost;
-using Machine.Specifications;
-using Procent.Redirector.Configuration;
+﻿using Machine.Specifications;
 
 namespace Procent.Redirector.Tests.Acceptance.service
 {
     public class when_requesting_root
+        : making_request
     {
-        Establish ctx = () =>
-            {
-                var address = new Uri("http://localhost:8355");
-                var config = new HttpSelfHostConfiguration(address);
-                WebApiBootstrap.Configure(config);
-
-                var server = new HttpSelfHostServer(config);
-
-                _httpClient = new HttpClient(server);
-                _httpClient.BaseAddress = address;
-            };
-
-        Because of = () => _response = _httpClient.GetAsync("").Result;
+        Establish ctx = () => _url = "";
 
         It returns_success_response = () => _response.IsSuccessStatusCode.ShouldBeTrue();
-
-        Cleanup stuff = () => _httpClient.Dispose();
-
-        static HttpClient _httpClient;
-        static HttpResponseMessage _response;
     }
 }
