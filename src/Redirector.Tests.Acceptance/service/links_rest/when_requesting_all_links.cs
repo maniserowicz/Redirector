b3f9e236e4d352dbca6b083adad959f8a9df2fe1
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2013-04-30 07:29:12 +0200

    added acceptance test for fetching all links via API

---
+++ src/Redirector.Tests.Acceptance/service/links_rest/when_requesting_all_links.cs
@@ -0,0 +1,39 @@
+﻿using Machine.Specifications;
+using Newtonsoft.Json;
+
+namespace Procent.Redirector.Tests.Acceptance.service.links_rest
+{
+    [Subject("API")]
+    public class when_requesting_all_links
+        : using_db
+    {
+        Establish ctx = () =>
+            {
+                _url = "links";
+
+                _link1 = new Link
+                {
+                    Alias = "first-link",
+                    Target = "http://first-target.com/",
+                };
+                _link2 = new Link
+                {
+                    Alias = "second-link",
+                    Target = "http://second-target.com/",
+                };
+
+                using (var session = _store.OpenSession())
+                {
+                    session.Store(_link1);
+                    session.Store(_link2);
+                    session.SaveChanges();
+                }
+            };
+
+        It returns_all_links_from_database = () => JsonConvert.DeserializeObject<Link[]>(_response.Content.ReadAsStringAsync().Result)
+            .ShouldBeLike(new[] { _link1, _link2 });
+
+        static Link _link1;
+        static Link _link2;
+    }
+}
