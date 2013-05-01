d3fdf36978f6c5265d18ea38f6308ad782e5a0e8
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2013-05-01 19:47:15 +0200

    deleted mvc routing - it's overriden by webapi anyway

--- src/Redirector/Global.asax.cs
+++ src/Redirector/Global.asax.cs
@@ -1,6 +1,5 @@
 ﻿using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Routing;
 using Procent.Redirector.Configuration;
 
 namespace Procent.Redirector
@@ -12,7 +11,6 @@ namespace Procent.Redirector
             AreaRegistration.RegisterAllAreas();
 
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             Bootstraper.ConfigureWebApi(GlobalConfiguration.Configuration);
 
