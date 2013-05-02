eb67ca45c3aa4c806bf3c774af415f3b27a1f3fd
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2013-05-02 14:09:08 +0200

    added acceptance tests for requesting single link via API

---
+++ src/Redirector.Tests.Acceptance/service/links_rest/requesting_single_link.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using Machine.Specifications;
+using Newtonsoft.Json;
+
+namespace Procent.Redirector.Tests.Acceptance.service.links_rest
+{
+    public abstract class requesting_single_link
+        : using_db
+    {
+        Establish ctx = () =>
+            {
+                link = new Link
+                    {
+                        Alias = "link-alias",
+                        Target = "http://target.com/",
+                        Visits = new List<Visit>
+                            {
+                                new Visit
+                                    {
+                                        Occured = DateTime.UtcNow,
+                                        Referrer = null,
+                                    },
+                                new Visit
+                                    {
+                                        Occured = DateTime.UtcNow,
+                                        Referrer = null,
+                                    }
+                            }
+                    };
+
+                using (var session = store.OpenSession())
+                {
+                    session.Store(link);
+                    session.SaveChanges();
+                }
+            };
+
+        protected static Link link;
+    }
+
+    [Subject("API")]
+    public class when_requesting_single_existing_link
+        : requesting_single_link
+    {
+        Establish ctx = () =>
+            {
+                url = "links/" + link.Id;
+            };
+
+        It returns_link_from_database = () => DeserializedResponse.ShouldBeLike(link);
+
+
+        static Link DeserializedResponse
+        {
+            get { return JsonConvert.DeserializeObject<Link>(response.Content.ReadAsStringAsync().Result); }
+        }
+    }
+
+    [Subject("API")]
+    public class when_requesting_single_nonexisting_link
+        : requesting_single_link
+    {
+        Establish ctx = () =>
+            {
+                url = "links/nonexisting-link";
+            };
+
+        It returns_404_not_found = () => response.StatusCode.ShouldEqual(HttpStatusCode.NotFound);
+    }
+}
