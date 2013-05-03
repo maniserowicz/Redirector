0215f90a36012560dc9d59b7e26e0b46a3b25240
Maciej Aniserowicz <maciej.aniserowicz@gmail.com>
2013-05-03 08:54:40 +0200

    added jquery and angular to main page

---
+++ src/Redirector/Scripts/app/home.js
@@ -0,0 +1,6 @@
+﻿function LinksCtrl($scope) {
+    $scope.links = [
+        { Alias: 'google', Target: 'http://www.google.com' },
+        { Alias: 'youtube', Target: 'http://www.youtube.com'}
+    ];
+}
